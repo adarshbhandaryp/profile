51d0c46a89ed2c2355c2d028aac86735fa1f776a
abp <adarshbhandaryp@gmail.com>
2026-01-07 15:05:36 +0100

    Refactor: Move gallery data to src/data/gallery.ts to fix Astro compiler panic

---
+++ src/data/gallery.ts
@@ -0,0 +1,27 @@
+export const talkPhotos = [
+  {
+    src: "/profile/bvm2024.jpeg",
+    alt: "Oral Presentation on Attention-Guided Erasing at BVM 2024, Erlangen, Germany",
+    caption: "Oral Presentation on Attention-Guided Erasing at BVM 2024"
+  },
+  {
+    src: "/profile/cvip2025.jpeg",
+    alt: "Oral Presentation on Dataset-Informed Transfer Learning (DITL) at CVIP 2025, Punjab, IIT Ropar, India",
+    caption: "Oral Presentation on DITL at CVIP 2025, IIT Ropar, India"
+  },
+  {
+    src: "/profile/ieee.jpeg",
+    alt: "Oral Presentation on MammoBLIP at IEEE Medical Imaging Conference (MIC) 2025, Yokohama, Japan",
+    caption: "Oral Presentation on MammoBLIP at IEEE MIC 2025, Yokohama"
+  },
+  {
+    src: "/profile/spie.jpeg",
+    alt: "Poster Presentation at Medical Imaging 2024: Computer-Aided Diagnosis (SPIE)",
+    caption: "Poster Presentation at SPIE Medical Imaging 2024"
+  },
+  {
+    src: "/profile/prs2025.jpeg",
+    alt: "Presentation at Pattern Recognition Symposium, Austria",
+    caption: "Pattern Recognition Symposium, Austria"
+  }
+];
