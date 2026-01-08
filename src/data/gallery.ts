4ae64e1dc8ac0721aeb9d2425cb02e514bb573dc
abp <adarshbhandaryp@gmail.com>
2026-01-08 12:25:52 +0100

    Changes to directory structure

--- src/data/gallery.ts
+++ src/data/gallery.ts
@@ -1,26 +1,26 @@
 export const talkPhotos = [
     {
-        src: "/profile/public/bvm2024.jpeg",
+        src: "/home/bvm2024.jpeg",
         alt: "Oral Presentation on Attention-Guided Erasing at BVM 2024, Erlangen, Germany",
         caption: "Oral Presentation on Attention-Guided Erasing at BVM 2024, Erlangen, Germany"
     },
     {
-        src: "/profile/public/cvip2025.jpeg",
+        src: "/home/cvip2025.jpeg",
         alt: "Oral Presentation on Dataset-Informed Transfer Learning (DITL) at CVIP 2025, Punjab, IIT Ropar, India",
         caption: "Oral Presentation on DITL at CVIP 2025, IIT Ropar, India"
     },
     {
-        src: "/profile/public/ieee.jpeg",
+        src: "/home/ieee.jpeg",
         alt: "Oral Presentation on MammoBLIP at IEEE Medical Imaging Conference (MIC) 2025, Yokohama, Japan",
         caption: "Oral Presentation on MammoBLIP at IEEE MIC 2025, Yokohama"
     },
     {
-        src: "/profile/public/spie.jpeg",
+        src: "/home/spie.jpeg",
         alt: "Poster Presentation at Medical Imaging 2024: Computer-Aided Diagnosis (SPIE)",
         caption: "Poster Presentation at SPIE Medical Imaging 2024"
     },
     {
-        src: "/profile/public/prs2025.jpeg",
+        src: "/home/prs2025.jpeg",
         alt: "Presentation at Pattern Recognition Symposium, Austria",
         caption: "Pattern Recognition Symposium, Austria"
     }
